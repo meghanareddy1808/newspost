6a9c2ae1ea9ebfc98b6628d1a9193c79fdd63c62
meghanareddy1808 <108571707+meghanareddy1808@users.noreply.github.com>
2023-02-19 22:42:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+from bs4 import BeautifulSoup
+import requests
+app=Flask(__name__)
+@app.route('/',methods=["GET","POST"])
+def index():
+
+    url="https://www.businesstoday.in/technology"
+    req=requests.get(url)
+    soup=BeautifulSoup(req.content,"html.parser")
+    Outerdata=soup.find_all("div",class_="widget-listing",limit=6)
+    finalNews=""
+    for data in Outerdata:
+        news=data.div.div.a["title"]
+        finalNews+="\u2022 "+news+"\n"
+    return render_template("index.html",News=finalNews)
+
+   
